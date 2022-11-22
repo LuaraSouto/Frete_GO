ca43346ceefe482cf6d2812565577c31c8d9259e
LuaraSouto <109703333+LuaraSouto@users.noreply.github.com>
2022-11-22 16:03:54 -0300

    tentativa 1 de colocar um mapa
    primeira tentativa deu errado

--- App/src/screens/Home.js
+++ App/src/screens/Home.js
@@ -11,7 +11,7 @@ const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+  
        style={styles.map}
        region={{
          latitude: 37.78825,
@@ -31,14 +31,14 @@ const Home = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
+
     height: 400,
     width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    height: '60%'
   },
   text: {
     fontSize: 30,
